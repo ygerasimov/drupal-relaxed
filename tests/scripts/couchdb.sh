2bf8d5f96c16ce0060527c541d68467ef602ea6e
Andrei Jechiu <andrei.jechiu@gmail.com>
2014-10-22 11:16:26 +0300

    Travis script fixes.

--- tests/scripts/couchdb.sh
+++ tests/scripts/couchdb.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-script:
-  # Enable Simpletest.
-  - cd $TRAVIS_BUILD_DIR/../drupal
-  - drush en --yes simpletest
-  - drush cr
-  # Create a new CouchDB database.
-  - curl -X PUT localhost:5984/test_db
-  - drush --yes pm-enable relaxed_test || true
+# Enable Simpletest.
+- cd $TRAVIS_BUILD_DIR/../drupal
+- drush en --yes simpletest
+- drush cr
+# Create a new CouchDB database.
+- curl -X PUT localhost:5984/test_db
+- drush --yes pm-enable relaxed_test || true
