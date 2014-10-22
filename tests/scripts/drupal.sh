500e2cff7bc02d6dcd12108269c4c20d0424dfda
Andrei Jechiu <andrei.jechiu@gmail.com>
2014-10-22 11:28:10 +0300

    Travis script fixes.

--- tests/scripts/drupal.sh
+++ tests/scripts/drupal.sh
@@ -2,12 +2,12 @@
 
 
 # Enable Simpletest.
-- cd $TRAVIS_BUILD_DIR/../drupal
-- drush en --yes simpletest
-- drush cr
-- php core/scripts/run-tests.sh --verbose --color --concurrency 4 --php `which php` --url http://localhost "relaxed" | tee /tmp/test.txt
-- export TEST_EXIT=${PIPESTATUS[0]}
+cd $TRAVIS_BUILD_DIR/../drupal
+drush en --yes simpletest
+drush cr
+php core/scripts/run-tests.sh --verbose --color --concurrency 4 --php `which php` --url http://localhost "relaxed" | tee /tmp/test.txt
+export TEST_EXIT=${PIPESTATUS[0]}
 # Simpletest does not exit with code 0 on success, so we will need to analyze
 # the output to ascertain whether the tests passed.
-- TEST_SIMPLETEST=$(! egrep -i "([0-9]+ fails)|(PHP Fatal error)|([0-9]+ exceptions)" /tmp/test.txt > /dev/null)$?
-- if [ $TEST_EXIT -eq 0 ] && [ $TEST_SIMPLETEST -eq 0 ]; then exit 0; else exit 1; fi
+TEST_SIMPLETEST=$(! egrep -i "([0-9]+ fails)|(PHP Fatal error)|([0-9]+ exceptions)" /tmp/test.txt > /dev/null)$?
+if [ $TEST_EXIT -eq 0 ] && [ $TEST_SIMPLETEST -eq 0 ]; then exit 0; else exit 1; fi
