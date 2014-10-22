049df502bbc3327a0373e7947ac12598015e4ba0
Andrei Jechiu <andrei.jechiu@gmail.com>
2014-10-22 16:44:01 +0300

    Create a test user and try to fix database access error.

--- tests/scripts/couchdb.sh
+++ tests/scripts/couchdb.sh
@@ -6,7 +6,7 @@ curl -X PUT localhost:5984/test_db
 # Enable Simpletest.
 drush en --yes simpletest
 drush cr
-curl -v -H "Content-Type:application/json" -X POST -d '{"source":"http://localhost/relaxed/default","target":"localhost:5984/test_db"}' http://localhost:5984/_replicate | tee /tmp/test_couchdb.txt
+curl -v -H "Content-Type:application/json" -X POST -d '{"source":"http://test_user:pass@localhost/relaxed/default","target":"localhost:5984/test_db"}' http://localhost:5984/_replicate | tee /tmp/test_couchdb.txt
 export TEST_EXIT=${PIPESTATUS[0]}
 # Analyze the output to ascertain whether the tests passed.
 TEST_COUCHDB=$(! egrep -i "(error)" /tmp/test_couchdb.txt > /dev/null)$?
