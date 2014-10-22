3f2301b9546b0788657af0ee6562125acd38095b
Andrei Jechiu <andrei.jechiu@gmail.com>
2014-10-22 15:03:41 +0300

    Correct the comments.

--- tests/scripts/couchdb.sh
+++ tests/scripts/couchdb.sh
@@ -8,7 +8,6 @@ drush en --yes simpletest
 drush cr
 curl -v -H "Content-Type:application/json" -X POST -d '{"source":"http://localhost/relaxed/default","target":"localhost:5984/test_db"}' http://localhost:5984/_replicate | tee /tmp/test_couchdb.txt
 export TEST_EXIT=${PIPESTATUS[0]}
-# Simpletest does not exit with code 0 on success, so we will need to analyze
-# the output to ascertain whether the tests passed.
+# Analyze the output to ascertain whether the tests passed.
 TEST_COUCHDB=$(! egrep -i "(error)" /tmp/test_couchdb.txt > /dev/null)$?
 if [ $TEST_EXIT -eq 0 ] && [ $TEST_COUCHDB -eq 0 ]; then exit 0; else exit 1; fi
