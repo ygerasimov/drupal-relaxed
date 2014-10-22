f2428574ad62bc415fcf3a8115f02d1af04aadfc
Andrei Jechiu <andrei.jechiu@gmail.com>
2014-10-22 14:05:35 +0300

    Set the Content-Type.

--- tests/scripts/couchdb.sh
+++ tests/scripts/couchdb.sh
@@ -6,4 +6,4 @@ curl -X PUT localhost:5984/test_db
 # Enable Simpletest.
 drush en --yes simpletest
 drush cr
-curl -X POST -d '{"source":"http://localhost/relaxed/default","target":"localhost:5984/test_db"}' http://localhost:5984/_replicate
+curl -v -H "Content-Type:application/json" -X POST -d '{"source":"http://localhost/relaxed/default","target":"localhost:5984/test_db"}' http://localhost:5984/_replicate
