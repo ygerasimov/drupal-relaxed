bf4e65ceb60b297ea63f58026b99ea518c69cd3c
Andrei Jechiu <andrei.jechiu@gmail.com>
2014-10-23 12:12:16 +0300

    Log headers in Apache2.

--- tests/scripts/couchdb.sh
+++ tests/scripts/couchdb.sh
@@ -8,6 +8,7 @@ drush en --yes simpletest
 drush cr
 curl -v -H "Accept:application/json" -H "Content-Type:application/json" -X POST -d '{"source":"http://user:pass@localhost/relaxed/default","target":"localhost:5984/test_db"}' http://localhost:5984/_replicate | tee /tmp/test_couchdb.txt
 sudo cat /var/log/apache2/access.log
+sudo cat /var/log/apache2/forensic.log
 export TEST_EXIT=${PIPESTATUS[0]}
 # Analyze the output to ascertain whether the tests passed.
 TEST_COUCHDB=$(! egrep -i "(error)" /tmp/test_couchdb.txt > /dev/null)$?
