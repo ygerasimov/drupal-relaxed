4b326812d3eda2b6bb00e2728e5347a68794c62f
Andrei Jechiu <andrei.jechiu@gmail.com>
2014-10-23 13:56:04 +0300

    Check if the frontpage can be requested.

--- tests/scripts/couchdb.sh
+++ tests/scripts/couchdb.sh
@@ -6,6 +6,10 @@ curl -X PUT localhost:5984/test_db
 # Enable Simpletest.
 drush en --yes simpletest
 drush cr
+
+# Check if the frontpage can be requested.
+curl -X GET localhost
+
 curl -v -H "Accept:application/json" -H "Content-Type:application/json" -X POST -d '{"source":"http://user:pass@localhost/relaxed/default","target":"localhost:5984/test_db"}' http://localhost:5984/_replicate | tee /tmp/test_couchdb.txt
 sudo cat /var/log/apache2/access.log
 sudo cat /var/log/apache2/forensic.log
